91a0df2d353ba7c1e377ab4573f848aee5cdc136
Paul Milne <hallhill@gmail.com>
2016-10-02 13:06:21 +0100

    fixed add and remove

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,11 +1,11 @@
 def pet_shop_name(array)
-    @pet_shop[:name]
+    array[:name]
 end
 
 def total_cash(array)
-    @pet_shop[:admin][:total_cash]
+    array[:admin][:total_cash]
 end
 
-def add_or_remove_cash_add(array, addition)
-    @pet_shop[:admin][:total_cash] + addition
+def add_or_remove_cash(array, amount)
+    array[:admin][:total_cash] = total_cash(array) + amount
 end
