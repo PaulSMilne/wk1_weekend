1ce6c945720b278337c694cc9c56bf2598b47810
Paul Milne <hallhill@gmail.com>
2016-10-02 20:31:51 +0100

    pets_by_breed x 2

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,27 +1,40 @@
-def pet_shop_name(array)
-    array[:name]
+def pet_shop_name(hash)
+    hash[:name]
 end
 
-def total_cash(array)
-    array[:admin][:total_cash]
+def total_cash(hash)
+    hash[:admin][:total_cash]
 end
 
-def add_or_remove_cash(array, amount)
-    array[:admin][:total_cash] = total_cash(array) + amount
+def add_or_remove_cash(hash, amount)
+    hash[:admin][:total_cash] = total_cash(hash) + amount
 end
 
-def pets_sold(array)
-    array[:admin][:pets_sold]
+def pets_sold(hash)
+    hash[:admin][:pets_sold]
 end
 
-def increase_pets_sold(array, amount)
-    array[:admin][:pets_sold] = pets_sold(array) + amount
+def increase_pets_sold(hash, amount)
+    hash[:admin][:pets_sold] = pets_sold(hash) + amount
 end
 
-def stock_count(array)
+def stock_count(hash)
     counter=0
-    for pet in array[:pets]
+    for pet in hash[:pets]
         counter += 1
     end
     counter
 end
+
+def pets_by_breed(hash, pet_breed)
+    breed_array = []
+    counter = 0
+    for pet in hash
+        if hash[:pets][counter][:breed] == pet_breed
+            breed_array << pet_breed
+        end
+        counter +=1
+    end
+    return breed_array
+end
+
