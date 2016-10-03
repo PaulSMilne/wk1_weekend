1f4a6ad75e0d81cda0f20d2ab5ec7d6fe9c3c226
Paul Milne <hallhill@gmail.com>
2016-10-03 09:21:48 +0100

    find_pet_by_name and add_pet_to_stock

--- pet_shop.rb
+++ pet_shop.rb
@@ -29,12 +29,32 @@ end
 def pets_by_breed(hash, pet_breed)
     breed_array = []
     counter = 0
-    for pet in hash
+    for pet in hash[:pets]
         if hash[:pets][counter][:breed] == pet_breed
-            breed_array << pet_breed
+            breed_array << hash[:pets][counter]
         end
-        counter +=1
+        counter += 1
     end
     return breed_array
+end
+
+def find_pet_by_name(hash, pet_name) 
+    match = nil
+    for pet in hash[:pets]
+        if pet[:name] == pet_name
+            match = pet
+        end
+    end
+    return match
+end
+
+def remove_pet_by_name(hash, pet_name)
+    for pet in hash[:pets]
+        hash[:pets].delete_if { |pet| pet[:name] == pet_name}
+    end
+end
+
+def add_pet_to_stock(hash, new_pet)
+    hash[:pets] << new_pet
 end
 
